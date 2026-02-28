fedd34226a608cc9ba668607a7ebc3a6fe443aa2
Nicormu <nicorojasm10@gmail.com>
2026-02-28 12:09:48 -0500

    Walk/Run Anim

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -8,7 +8,10 @@ public class PlayerMovemente : MonoBehaviour
 
     private float movementX;
 
+    private SpriteRenderer sr;
     private Rigidbody2D rb;
+    private Animator animator;
+
     public float jumpForce = 10.0f;
 
     public bool isColliding = false;
@@ -17,12 +20,25 @@ public class PlayerMovemente : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
         movementX = Input.GetAxisRaw("Horizontal");
+
+        if (movementX < 0)
+        {
+            sr.flipX = true;
+        }
+        else if (movementX > 0)
+        {
+            sr.flipX = false;
+        }
+        
+        AnimatorManager();
         Sprint();
         rb.linearVelocity = new Vector2(movementX * speed, rb.linearVelocity.y);
         if (Input.GetKeyDown(KeyCode.Space)){
@@ -64,8 +80,26 @@ public class PlayerMovemente : MonoBehaviour
         {
             speed = minspeed;
         }
-
         return speed;
+    }
+
+    void AnimatorManager()
+    {
+        if (movementX == 0)
+        {
+            animator.SetBool("isWalking", false);
+            animator.SetBool("isRunning", false);
+        }
+        else if (movementX != 0 && Input.GetKey(KeyCode.LeftShift))
+        {
+            animator.SetBool("isWalking", false);
+            animator.SetBool("isRunning", true);
+        }
+        else
+        {
+            animator.SetBool("isWalking", true);
+            animator.SetBool("isRunning", false);
+        }
     }
 
 }
